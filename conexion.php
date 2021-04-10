c1f1089a47b74abb8261968e7a6e77978b2a5677
Mano-Drop <manuel.aleajandro@gmail.com>
2021-04-10 19:48:18 -0400

    Este CRUD es un proyecto que sera entregado por github
    Signed-off-by: Mano-Drop <manuel.aleajandro@gmail.com>

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
 
 $host = "localhost";
-$user = "stwar";
-$pass = "r2d2f0-2021";
+$user = "Manuel";
+$pass = "Elpilar-2021";
 $db = "Gym";
 
 $conexion = mysqli_connect($host, $user, $pass, $db);
