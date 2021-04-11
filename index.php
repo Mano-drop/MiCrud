5f5898fbf530ad5fc3048fd0ecbf755cb7b6f64c
Mano-Drop <manuel.aleajandro@gmail.com>
2021-04-11 18:53:03 -0400

    Mejora al Index
    Signed-off-by: Mano-Drop <manuel.aleajandro@gmail.com>

--- index.php
+++ index.php
@@ -27,5 +27,31 @@
 </form>
 </div>
 
+<div style=" text-align: center;"> <h3>Consulta De Clientes del Gym</h3></div>
+
+<?php
+//conexion con la base de datos
+include("conexion.php");
+
+$solicitud = "SELECT * FROM clientes";
+$resultado = mysqli_query($conexion, $solicitud);
+
+echo "<table border='1' align='center'> <tr><td bgcolor='#87cefa'>Nombre y Apellido</td><td bgcolor='#87cefa'>Cedula</td><td bgcolor='#87cefa'>Celular</td><td bgcolor='#87cefa'; colspan='2'>Opciones</td></tr>";
+
+while($fila=mysqli_fetch_array($resultado)){
+
+    echo "<tr>";
+    echo "<td>" . $fila['Nombre'] ." " . $fila['Apellido'] . ",</td>";
+    echo "<td>" . $fila['Cedula']  . "</td>";
+    echo "<td>" . $fila['Celular']  . "</td>";
+    echo "<td><a href='eliminar.php?id=".$fila['ID']." '>Eliminar</a></td>";
+    echo "<td><a href='selecionar.php?id=".$fila['ID']." '>Modificar</a></td>";
+    echo "</tr>";
+}
+
+
+echo "</table>"
+
+?>
 </body>
 </html>
